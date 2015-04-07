8a89d16ab40e163eb69b0f2b3e1d07574c9a6069
root <root@docker1>
2015-04-07 20:48:32 -0300

    ajustes script

--- zabbix_mysql/bin/mysql.sh
+++ zabbix_mysql/bin/mysql.sh
@@ -22,7 +22,7 @@ UPDATE user SET password=PASSWORD("$MYSQL_ROOT_PASSWORD") WHERE user='root';
 EOF
 
 if [[ $MYSQL_DATABASE != "" ]]; then
-    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER set utf8 collate utf8_bin;" >> $tfile
 
     if [[ $MYSQL_USER != "" ]]; then
         echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
--- zabbix_server/sh/zabbix.sh
+++ zabbix_server/sh/zabbix.sh
@@ -4,6 +4,25 @@ Z_HOME="/opt/zabbix"
 Z_CONF="${Z_HOME}/etc/zabbix_server.conf"
 Z_LOG=$(grep ^LogFile= ${Z_CONF}| cut -d= -f2)
 
+
+sed -i s/^DBHost=.*/DBHost=${MYSQL_PORT_3306_TCP_ADDR}/ ${Z_CONF}
+sed -i s/^DBUser=.*/DBUser=${ZABBIX_USER}/ ${Z_CONF}
+sed -i s/^DBName=.*/DBName=${ZABBIX_DB}/ ${Z_CONF}
+sed -i s/^DBPassword=.*/DBPassword=${ZABBIX_PASS}/ ${Z_CONF}
+
+mysql -u${ZABBIX_USER} -p${ZABBIX_PASS} \
+        -h${MYSQL_PORT_3306_TCP_ADDR} ${ZABBIX_DB} \
+        -e "select * from users" &> /dev/null
+
+if [ $? -ne 0 ] ; then
+        echo "Criando a estrutura do banco Zabbix"
+        mysql -u${ZABBIX_USER} -p${ZABBIX_PASS} \
+        -h${MYSQL_PORT_3306_TCP_ADDR} ${ZABBIX_DB} < ${Z_SRC}/zabbix-${Z_VERSION}/database/mysql/schema.sql
+        mysql -u${ZABBIX_USER} -p${ZABBIX_PASS} \
+        -h${MYSQL_PORT_3306_TCP_ADDR} ${ZABBIX_DB} < ${Z_SRC}/zabbix-${Z_VERSION}/database/mysql/images.sql
+        mysql -u${ZABBIX_USER} -p${ZABBIX_PASS} \
+        -h${MYSQL_PORT_3306_TCP_ADDR} ${ZABBIX_DB} < ${Z_SRC}/zabbix-${Z_VERSION}/database/mysql/data.sql  
+fi
+
 ${Z_HOME}/sbin/zabbix_server -c ${Z_CONF}
 tail -f ${Z_LOG}
-
